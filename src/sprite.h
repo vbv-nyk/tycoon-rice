bb776916c9011e0b9178db28c4e633f39f6a4eca
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2025-06-22 12:00:54 +0530

    Adding memebers to a stages of growth

--- src/sprite.h
+++ src/sprite.h
@@ -1,20 +1,21 @@
+#include <string>
 #include <vector>
 class Sprite {
-    class Stages {
+    class Stage {
+       private:
+        int required_age;
+        std::string sprite_image;
+
+       public:
+        Stage(int _required_age, const std::string& _sprite_image) : required_age(_required_age), sprite_image(sprite_image) {}
     };
 
-    Sprite(int _age, std::vector<Stages> _stages) {
-        age = _age;
-        for (int i = 0; i < _stages.size(); i++) {
-            stages.push_back(_stages[i]);
-        }
-    }
-
    private:
-    void check_growth();
     int age;
-    std::vector<Stages> stages;
+    std::vector<Stage> stages;
 
    public:
     int get_age();
+    void age_forward();
+    Sprite(int _age, const std::vector<Stage>& _stages) : age(_age), stages(_stages) {}
 };
